3239a356d995fc9d6bb0f07613d5d0ed6987b9c8
lanseria <zhangchao564265135@hotmail.com>
2018-03-09 11:16:46 +0800

    add console.log

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ apiRouters.post('/one', function (req, res) {
       'Cookie': 'bdshare_firstime=1520491425080; cck_lasttime=1520491425990; cck_count=0; netbei=1; Hm_lvt_5a01e7efb58f3593053ded683ad004fd=1520491425,1520491863; Hm_lvt_f6d4708d5f4f9ba8ecdb2dd004c2623e=1520491426,1520491864; Hm_lpvt_f6d4708d5f4f9ba8ecdb2dd004c2623e=1520491864; Hm_lpvt_5a01e7efb58f3593053ded683ad004fd=1520491916'
     }
   }).then((response) => {
+    console.log(response)
     res.send(response.data)
     // res.send(req.query.jsonpCallback + '(' + JSON.stringify(response.data) + ')')
   }).catch(e => {
