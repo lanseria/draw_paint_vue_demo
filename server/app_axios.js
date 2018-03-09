04156532e12d7293f1a8976347df6f57130b6b54
lanseria <zhangchao564265135@hotmail.com>
2018-03-09 13:41:55 +0800

    rename

--- server/app_axios.js
+++ server/app_axios.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 var qs = require('qs');
-var got = require('got')
-var express = require('express')
+var axios = require('axios');
+var express = require('express');
 var bodyParser = require('body-parser');
 
-var port = 7999
-var app = express()
-var apiRouters = express.Router()
+var port = 7999;
+var app = express();
+var apiRouters = express.Router();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 apiRouters.post('/one', function (req, res) {
   var url = 'http://www.yibizi.com/html5/hd8.php'
-  got.post(url, {
+  axios.post(url, qs.stringify({...req.body}), {
     headers: {
       referer: 'http://www.yibizi.com/html5/',
       'X-DevTools-Emulate-Network-Conditions-Client-Id': '(10954F5817FE8E4787E42891F4F61524)',
@@ -22,21 +22,19 @@ apiRouters.post('/one', function (req, res) {
       'Content-Type': 'application/x-www-form-urlencoded',
       'DNT': '1',
       'Cookie': 'bdshare_firstime=1520491425080; cck_lasttime=1520491425990; cck_count=0; netbei=1; Hm_lvt_5a01e7efb58f3593053ded683ad004fd=1520491425,1520491863; Hm_lvt_f6d4708d5f4f9ba8ecdb2dd004c2623e=1520491426,1520491864; Hm_lpvt_f6d4708d5f4f9ba8ecdb2dd004c2623e=1520491864; Hm_lpvt_5a01e7efb58f3593053ded683ad004fd=1520491916'
-    },
-    body: qs.stringify({ ...req.body })
+    }
   }).then((response) => {
-    console.log(response.body)
-    res.send(response.body)
+    console.log(response);
+    res.send(response.data);
     // res.send(req.query.jsonpCallback + '(' + JSON.stringify(response.data) + ')')
   }).catch(e => {
-    console.log(e)
+    console.log(e);
   })
 })
-app.use('/api', apiRouters)
+app.use('/api', apiRouters);
 
-module.exports = app
+module.exports = app;
 
 app.listen(port, function () {
-  console.log(`app is listening at port ${port}\nAPI转发服务已开启`)
-})
-
+  console.log(`app is listening at port ${port}\nAPI转发服务已开启`);
+});
