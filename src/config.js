a006cac0af0dc4e47915a93e04acae93e973406f
lanseria <zhangchao564265135@hotmail.com>
2018-03-19 18:16:24 +0800

    split production and dev

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const url = process.env.NODE_ENV === 'production' ? '//api.limonplayer.cn/jsonp/drawpaint/' : '//localhost:7999/'
--- src/utils/getWords.js
+++ src/utils/getWords.js
@@ -1,11 +1,10 @@
 import jsonp from '../utils/jsonp';
-
+import { url } from '../config';
 const options = {
   param: 'callback'
 }
 
 export function getWords (bhData) {
-  const url = '//localhost:7999/'
   const data = { ...bhData }
   return jsonp(url, data, options)
 }
